673b1904888e1862e25b85a045ad69e1cc7aa45d
Maisa <maysaasalaheldin@gmail.com>
2023-03-16 06:59:25 -0700

    modified files

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,64 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  
+
+
+  afterTakeOff: boolean = false;
+   takeOff: boolean = false;
+   outOfBounds: boolean[] = [false, false, false, false];
+  handleTakeOff(rocketImage) {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+       this.takeOff = true;
+       this.afterTakeOff = true;
+       let movement = parseInt(rocketImage.style.bottom) + 50 + 'px';
+         rocketImage.style.bottom = movement;
+    }
+ }
+ handleLand(){
+  window.alert("The shuttle is landing. Landing gear engaged.");
+  this.message= "The shuttle has landed.";
+  this.color='green'
+  this.height=0;
+ }
+
+ handleAbort(){
+ let result= window.confirm("Do you want to abort the mission?");
+ if(result){
+
+ 
+  this.message= "Mission aborted.";
+  this.color='red'
+  this.height=0;
+ }
+ }
+ moveRocket(rocketImage, direction:string){
+  if (direction === 'right') {
+    let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+    rocketImage.style.left = movement;
+    this.width = this.width + 10000;
+ }
+ if (direction === 'left') {
+  let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+  rocketImage.style.left = movement;
+  this.width = this.width + 10000;
+}
+if (direction === 'up') {
+  let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+  rocketImage.style.bottom = movement;
+  //this.width = this.width + 10000;
+  this.height=this.height+10000;
+}
+if (direction === 'down') {
+  let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+  rocketImage.style.bottom = movement;
+  this.width = this.width + 10000;
+  this.height=this.height-10000;
+}
+ }
 }
